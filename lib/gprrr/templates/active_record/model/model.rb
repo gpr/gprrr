5861ecd85b0252dc0c927f8e84b9f5265a620c3f
Gregory Romé <gregory.rome@pace.com>
2015-07-23 18:52:48 +0200

    templates: adds rails_admin to default model

--- lib/gprrr/templates/active_record/model/model.rb
+++ lib/gprrr/templates/active_record/model/model.rb
@@ -6,5 +6,61 @@ class <%= class_name %> < <%= parent_class_name.classify %>
 <% if attributes.any?(&:password_digest?) -%>
   has_secure_password
 <% end -%>
-end
+
+    # -----------------------------------------------------
+    # Instance methods
+
+
+    # -----------------------------------------------------
+    # Class methods
+
+
+    # -----------------------------------------------------
+    # Private methods
+    private
+
+    # -----------------------------------------------------
+    # rails_admin configuration
+    rails_admin do
+      list do
+      <%- attributes.each do |attribute| -%>
+        field :<%= attribute.name %>
+      <% end -%>
+      end
+      show do
+      <%- attributes.each do |attribute| -%>
+        field :<%= attribute.name %>
+      <% end -%>
+      end
+      edit do
+      <%- attributes.each do |attribute| -%>
+        field :<%= attribute.name %>
+      <% end -%>
+      end
+
+      create do
+      <%- attributes.each do |attribute| -%>
+        field :<%= attribute.name %>
+      <% end -%>
+      end
+
+      update do
+      <%- attributes.each do |attribute| -%>
+        field :<%= attribute.name %>
+      <% end -%>
+      end
+
+      nested do
+      <%- attributes.each do |attribute| -%>
+        field :<%= attribute.name %>
+      <% end -%>
+      end
+
+      modal do
+      <%- attributes.each do |attribute| -%>
+        field :<%= attribute.name %>
+      <% end -%>
+      end
+    end
+  end
 <% end -%>
