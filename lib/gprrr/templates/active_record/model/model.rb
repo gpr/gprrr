43b47296982fca5f2b4b7997ef11c95195c9782b
Gregory Romé <gregory.rome@gmail.com>
2015-11-10 09:09:58 +0100

    chanes model templates

--- lib/gprrr/templates/active_record/model/model.rb
+++ lib/gprrr/templates/active_record/model/model.rb
@@ -1,17 +1,19 @@
 <% module_namespacing do -%>
 class <%= class_name %> < <%= parent_class_name.classify %>
+    # -----------------------------------------------------
+    # Constants
+
+    # -----------------------------------------------------
+    # Inclusion / Extension (acts_as)
+
+    # -----------------------------------------------------
+    # Associations
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
 <% end -%>
 <% if attributes.any?(&:password_digest?) -%>
   has_secure_password
 <% end -%>
-    # -----------------------------------------------------
-    # Constants
-
-    # -----------------------------------------------------
-    # Associations
-
     # -----------------------------------------------------
     # Validations
     <%- attributes.each do |attribute| -%>
